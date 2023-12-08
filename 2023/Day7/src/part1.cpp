df55b80e7f9a8d46604b19ff598b82b48f89a2ad
Nidal Guerouaja <22301275+Nidalone29@users.noreply.github.com>
2023-12-08 03:34:34 +0100

    2023 Day7 Part 1

---
+++ 2023/Day7/src/part1.cpp
@@ -0,0 +1,173 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <array>
+#include <algorithm>
+
+std::map<char, int> strength = {
+    {'A', 12}, {'K', 11}, {'Q', 10}, {'J', 9}, {'T', 8}, {'9', 7}, {'8', 6},
+    {'7', 5},  {'6', 4},  {'5', 3},  {'4', 2}, {'3', 1}, {'2', 0}};
+
+// already sorted
+enum HAND_TYPE {
+  HIGH_CARD,
+  ONE_PAIR,
+  TWO_PAIR,
+  THREE_OF_A_KIND,
+  FULL_HOUSE,
+  FOUR_OF_A_KIND,
+  FIVE_OF_A_KIND
+};
+
+class Hand {
+ public:
+  Hand(const std::string hand) {
+    if (hand.size() != 5) {
+      std::cerr << "error creating hand" << std::endl;
+      throw;
+    }
+    for (int i = 0; i < hand.size(); i++) {
+      _cards[i] = hand[i];
+    }
+    find_type();
+  }
+
+  const std::array<char, 5>& getCards() const {
+    return _cards;
+  }
+
+  const HAND_TYPE& getType() const {
+    return _type;
+  }
+
+  friend bool operator<(const Hand& l, const Hand& r) {
+    if (l._type < r._type) {
+      return true;
+    } else if (l._type > r._type) {
+      return false;
+    } else {  // l._type == r._type
+      for (int i = 0; i < l._cards.size();) {
+        if (strength[l._cards[i]] == strength[r._cards[i]]) {
+          i++;
+        } else if (strength[l._cards[i]] < strength[r._cards[i]]) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+
+  friend bool operator>(const Hand& l, const Hand& r) {
+    return r < l;
+  }
+
+  friend bool operator<=(const Hand& l, const Hand& r) {
+    return !(l > r);
+  }
+
+  friend bool operator>=(const Hand& l, const Hand& r) {
+    return !(l < r);
+  }
+
+ private:
+  void find_type() {
+    std::map<char, int> cards_count;
+    for (const char x : _cards) {
+      const auto [it_hinata, success] = cards_count.insert({x, 1});
+      if (!success) {
+        cards_count[x]++;
+      }
+    }
+
+    if (cards_count.size() == 1) {
+      _type = HAND_TYPE::FIVE_OF_A_KIND;
+    } else if (cards_count.size() == 2) {
+      for (const auto& [name, count] : cards_count) {
+        if (count == 1 || count == 4) {
+          _type = HAND_TYPE::FOUR_OF_A_KIND;
+          break;
+        } else {
+          _type = HAND_TYPE::FULL_HOUSE;
+          break;
+        }
+      }
+    } else if (cards_count.size() == 3) {
+      bool found_3 = false;
+      for (const auto& [name, count] : cards_count) {
+        if (count == 3) {
+          _type = HAND_TYPE::THREE_OF_A_KIND;
+          found_3 = true;
+          break;
+        }
+      }
+      if (!found_3) {
+        _type = HAND_TYPE::TWO_PAIR;
+      }
+    } else if (cards_count.size() == 4) {
+      _type = HAND_TYPE::ONE_PAIR;
+    } else if (cards_count.size() == 5) {
+      _type = HAND_TYPE::HIGH_CARD;
+    }
+  }
+
+  std::array<char, 5> _cards;
+
+  HAND_TYPE _type;
+};
+
+std::ostream& operator<<(std::ostream& stream, const Hand& other) {
+  stream << "type  " << other.getType() << "| cards "
+         << other.getCards().data();
+  return stream;
+}
+
+std::vector<std::string> split(const std::string& input, const std::string x) {
+  std::vector<std::string> output;
+  std::string to_tokenize = input;
+
+  while (to_tokenize.find(x) != std::string::npos) {
+    output.push_back(to_tokenize.substr(0, to_tokenize.find(x)));
+    to_tokenize = to_tokenize.substr(to_tokenize.find(x) + x.size());
+  }
+  output.push_back(to_tokenize);
+
+  return output;
+}
+
+int main() {
+  std::ifstream infile("input.txt");
+  std::vector<std::string> input;
+  // hand and bidding amount
+  std::vector<std::pair<Hand, int>> set_of_hands;
+
+  if (infile) {
+    std::cout << "opened input file" << std::endl;
+    std::string line;
+
+    while (getline(infile, line)) {
+      std::vector<std::string> splitted_line = split(line, " ");
+      set_of_hands.push_back(
+          {Hand(splitted_line[0]), std::stoi(splitted_line[1])});
+    }
+
+    infile.close();
+  } else {
+    std::cerr << "unable to open input file" << std::endl;
+  }
+
+  std::sort(set_of_hands.begin(), set_of_hands.end(),
+            [](std::pair<Hand, int> a, std::pair<Hand, int> b) {
+              return a.first < b.first;
+            });
+
+  int res = 0;
+  for (int i = 1; i <= set_of_hands.size(); i++) {
+    res += set_of_hands[i - 1].second * i;
+  }
+
+  std::cout << res << std::endl;
+  return 0;
+}
